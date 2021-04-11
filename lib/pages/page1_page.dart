ce0fd32e230356705f7af59b0fe945bcd4fbee94
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-04-11 11:51:55 -0500

    Se agrego una transicion con un Fade.

--- lib/pages/page1_page.dart
+++ lib/pages/page1_page.dart
@@ -29,15 +29,15 @@ class Page1Page extends StatelessWidget {
         Animation<double> animation, 
         Animation<double> secondaryAnimation
       ) => Page2Page(),
-      transitionDuration: Duration(seconds: 2),
-      reverseTransitionDuration: Duration(seconds: 2),
+      // transitionDuration: Duration(seconds: 2),
+      // reverseTransitionDuration: Duration(seconds: 2),
       transitionsBuilder: (
         context, 
         animation, 
         secondaryAnimation, 
         child
       ) {
-        final CurvedAnimation curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.bounceInOut);
+        final CurvedAnimation curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
         // return SlideTransition(
         //   position: Tween<Offset>(begin: Offset(0.5, 1.0), end: Offset.zero).animate(curvedAnimation),
         //   child: child,
@@ -46,8 +46,12 @@ class Page1Page extends StatelessWidget {
         //   child: child,
         //   scale: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
         // );
-        return RotationTransition(
-          turns: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
+        // return RotationTransition(
+        //   turns: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
+        //   child: child,
+        // );
+        return FadeTransition(
+          opacity: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
           child: child,
         );
       },
