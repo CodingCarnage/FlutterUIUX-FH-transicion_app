a056907f0fdd6424c22850888cb9fe1706951061
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-04-11 11:43:53 -0500

    Se agrego una nueva transicion.

--- lib/pages/page1_page.dart
+++ lib/pages/page1_page.dart
@@ -37,9 +37,13 @@ class Page1Page extends StatelessWidget {
         child
       ) {
         final CurvedAnimation curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
-        return SlideTransition(
-          position: Tween<Offset>(begin: Offset(0.5, 1.0), end: Offset.zero).animate(curvedAnimation),
+        // return SlideTransition(
+        //   position: Tween<Offset>(begin: Offset(0.5, 1.0), end: Offset.zero).animate(curvedAnimation),
+        //   child: child,
+        // );
+        return ScaleTransition(
           child: child,
+          scale: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
         );
       },
     );
